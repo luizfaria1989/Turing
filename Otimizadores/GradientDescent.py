306c37fc8973a9e8550ed7087a3e65f0c828a270
Luiz <luizguilermemoraiscosta@gmail.com>
2025-09-21 16:29:23 -0300

    atualiza documento do método do gradiente

--- Otimizadores/GradientDescent.py
+++ Otimizadores/GradientDescent.py
@@ -8,7 +8,7 @@ class GradientDescent:
     function: Representa a função que se quer descobrir o ponto de mínimo / otimizar
     functionPrime: Representa a derivada da função que se quer otimizar
     initialPoint: Representa o ponto inicial que o algoritmo irá começar o procurar pelo ponto de mínimo
-    maxIterations: Representa a quantidade máxima de iterações que o algoritmo irá aplicar o método
+    epochs: Representa a quantidade máxima de iterações que o algoritmo irá aplicar o método
     do gradiente
     tolerance: Representa a distância que a norma do gradiente estará de zero
     Retornos:
@@ -17,17 +17,17 @@ class GradientDescent:
     path: É uma lista que armazena os pontos que o modelo dá em direção ao ponto de mínimo da função
     """
 
-    def __init__(self, function, function_prime, initial_point, learning_rate=0.001, max_iterations=100, tolerance=1e-6):
+    def __init__(self, function, function_prime, initial_point, learning_rate=0.001, epochs=100, tolerance=1e-6):
         self.f = function
         self.fp = function_prime
         self.ip = initial_point
         self.lr = learning_rate
-        self.iterations = max_iterations
+        self.ep = epochs
         self.tol = tolerance
         self.path = []
 
     def update_step(self):
-        for i in range(self.iterations): # Inicia o Loop do Método do Gradiente
+        for i in range(self.ep): # Inicia o Loop do Método do Gradiente
             self.path.append(self.ip) # Adiciona o novo ponto na lista de pontos (path)
             grad = self.fp(self.ip) # Calcula o valor do vetor gradiente no ponto
             if abs(grad) < self.tol: break # se o gradiente for 0 então convergiu
