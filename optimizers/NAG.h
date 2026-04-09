53ddd008e5ba9ba59330ad91b283c4ab88b034f7
Luiz <luizguilermemoraiscosta@gmail.com>
2026-04-09 18:08:27 -0300

    feat: implementa otimizador nesterov accelereted gradient (NAG)

---
+++ optimizers/NAG.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "Optimizer.h"
+#include <unordered_map>
+
+class NAG : public Optimizer{
+
+private:
+    float momentum_;
+    std::unordered_map<const Eigen::MatrixXf*, Eigen::MatrixXf> velocities_;
+
+public:
+
+    NAG(float learning_rate = 0.01f, float momentum = 0.01f) : Optimizer(learning_rate), momentum_ (momentum) {}
+
+    virtual ~NAG() = default;
+
+    void Update (Eigen::MatrixXf &weights, Eigen::MatrixXf &biases, const Eigen::MatrixXf &grad_weights, const Eigen::MatrixXf &grad_biases) override {
+
+        if (velocities_.find(&weights) == velocities_.end()) {
+            velocities_[&weights] = Eigen::MatrixXf::Zero(weights.rows(), weights.cols());
+        }
+
+        if (velocities_.find(&biases) == velocities_.end()) {
+            velocities_[&biases] = Eigen::MatrixXf::Zero(biases.rows(), biases.cols());
+        }
+
+        velocities_[&weights] = (momentum_ * velocities_[&weights]) + (learning_rate_ * grad_weights);
+        weights = weights - (momentum_ * velocities_[&weights] + learning_rate_ * grad_weights);
+
+        velocities_[&biases] = (momentum_ * velocities_[&biases]) + (learning_rate_ * grad_biases);
+        biases = biases - (momentum_ * velocities_[&biases] + learning_rate_ * grad_biases);
+
+    };
+
+};
